8bed613e6bb84d4d48682cd56f0571ca265c45f2
EAinsley <vhtmscyo@gmail.com>
2020-07-29 17:21:31 +0800

    完成了练习4

---
+++ chapter4/e4.js
@@ -0,0 +1,11 @@
+function deepEqual(a, b) {
+    if (a && b && (typeof a == 'object') && typeof b == 'object') {
+        if (Object.keys(a).length != Object.keys(b).length) return false;
+        for (let key of Object.keys(a)) {
+            if (!(key in b) || !deepEqual(a[key], b[key])) return false;
+        }
+        return true;
+    } else {
+        return a === b;
+    }
+}
