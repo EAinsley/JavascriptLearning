93e07a1f461abd8de37987a4bff5e092174b1524
EAinsley <vhtmscyo@gmail.com>
2020-08-04 17:47:45 +0800

    Update e4.js

--- chapter5/e4.js
+++ chapter5/e4.js
@@ -1,5 +1,3 @@
-const SCRIPTS = require("./05_higher_order/code/scripts");
-
 require("./05_higher_order/code/load")("code/scripts.js", "code/chapter/05_higher_order.js", "code/intro.js");
 
 function dominantDirection(text) {
