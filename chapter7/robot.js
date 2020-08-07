fe74ceca280c4bc7d006a9ede358e2d85c99a53b
EAinsley <vhtmscyo@gmail.com>
2020-08-07 16:02:41 +0800

    开始学习第七章的内容。

---
+++ chapter7/robot.js
@@ -0,0 +1,16 @@
+function buildGraph(edges) {
+    let graph = Object.create(null); // 创建不以Object.prototype为原型的对象。
+    function addEdge(from, to) {
+        if (graph[from] == null) {
+            graph[from] = [to];
+        } else {
+            graph[from].push(to);
+        }
+    }
+
+    for (let [from, to] of edges.map(r => r.split("-"))) {
+        addEdge(from, to);
+        addEdge(to, from);
+    }
+    return graph;
+}
