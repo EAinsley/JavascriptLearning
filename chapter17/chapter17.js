6f7de08a2d044c6ca84acd99613aa8994574240a
EAinsley <vhtmscyo@gmail.com>
2021-07-16 16:03:02 +0800

    finished chapter 17 reading

--- chapter17/chapter17.js
+++ chapter17/chapter17.js
@@ -416,12 +416,11 @@ class Monster {
     let player = state.player;
     if (
       player.speed.y > 0 &&
-      Math.abs(player.pos.y + player.size.y - this.pos.y) < 0.1
+      Math.abs(player.pos.y + player.size.y - this.pos.y) < 0.5
     ) {
       let filtered = state.actors.filter((a) => a != this);
       return new State(state.level, filtered, state.status);
     } else {
-      console.log(Math.abs(player.pos.y + player.size.y - this.pos.y));
       return new State(state.level, state.actors, "lost");
     }
   }
@@ -460,6 +459,37 @@ class DOMDisplay {
   clear() {
     this.dom.remove();
   }
+  syncState(state) {
+    if (this.actorLayer) this.actorLayer.remove();
+    this.actorLayer = drawActors(state.actors);
+    this.dom.appendChild(this.actorLayer);
+    this.dom.className = `game ${state.status}`;
+    this.scrollPlayerIntoView(state);
+  }
+  scrollPlayerIntoView(state) {
+    let width = this.dom.clientWidth;
+    let height = this.dom.clientHeight;
+    let margin = width / 3;
+
+    let left = this.dom.scrollLeft;
+    let right = left + width;
+    let top = this.dom.scrollTop;
+    let bottom = top + height;
+
+    let player = state.player;
+    let center = player.pos.plus(player.size.times(0.5)).times(scale);
+
+    if (center.x < left + margin) {
+      this.dom.scrollLeft = center.x - margin;
+    } else if (center.x > right - margin) {
+      this.dom.scrollLeft = center.x + margin - width;
+    }
+    if (center.y < top + margin) {
+      this.dom.scrollTop = center.y - margin;
+    } else if (center.y > bottom - margin) {
+      this.dom.scrollTop = center.y + margin - height;
+    }
+  }
 }
 const scale = 20;
 function drawGrid(level) {
@@ -489,37 +519,6 @@ function drawActors(actors) {
     })
   );
 }
-DOMDisplay.prototype.syncState = function (state) {
-  if (this.actorLayer) this.actorLayer.remove();
-  this.actorLayer = drawActors(state.actors);
-  this.dom.appendChild(this.actorLayer);
-  this.dom.className = `game ${state.status}`;
-  this.scrollPlayerIntoView(state);
-};
-DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
-  let width = this.dom.clientWidth;
-  let height = this.dom.clientHeight;
-  let margin = width / 3;
-
-  let left = this.dom.scrollLeft;
-  let right = left + width;
-  let top = this.dom.scrollTop;
-  let bottom = top + height;
-
-  let player = state.player;
-  let center = player.pos.plus(player.size.times(0.5)).times(scale);
-
-  if (center.x < left + margin) {
-    this.dom.scrollLeft = center.x - margin;
-  } else if (center.x > right - margin) {
-    this.dom.scrollLeft = center.x + margin - width;
-  }
-  if (center.y < top + margin) {
-    this.dom.scrollTop = center.y - margin;
-  } else if (center.y > bottom - margin) {
-    this.dom.scrollTop = center.y + margin - height;
-  }
-};
 
 // tracking keys
 function trackKeys(keys) {
@@ -536,6 +535,157 @@ function trackKeys(keys) {
 }
 
 const arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
+
+// cavas drawing
+class CanvasDisplay {
+  constructor(parent, level) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = Math.min(600, level.width * scale);
+    this.canvas.height = Math.min(450, level.height * scale);
+    parent.appendChild(this.canvas);
+    this.cx = this.canvas.getContext("2d");
+    this.flipPlayer = false;
+    this.viewport = {
+      left: 0,
+      top: 0,
+      width: this.canvas.width / scale,
+      height: this.canvas.height / scale,
+    };
+  }
+  clear() {
+    this.canvas.remove();
+  }
+  syncState(state) {
+    this.updateViewport(state);
+    this.clearDisplay(state.status);
+    this.drawBackground(state.level);
+    this.drawActors(state.actors);
+  }
+  updateViewport(state) {
+    let view = this.viewport;
+    let margin = view.width / 3;
+    let player = state.player;
+    let center = player.pos.plus(player.size.times(0.5));
+    if (center.x < this.viewport.left + margin) {
+      view.left = Math.max(center.x - margin, 0);
+    } else if (center.x > view.left + view.width - margin) {
+      view.left = Math.min(
+        center.x + margin - view.width,
+        state.level.width - view.width
+      );
+    }
+    if (center.y < view.top + margin) {
+      view.top = Math.max(center.y - margin, 0);
+    } else if (center.y > view.top + view.height - margin) {
+      view.top = Math.min(
+        center.y + margin - view.height,
+        state.level.height - view.height
+      );
+    }
+  }
+  clearDisplay(status) {
+    if (status == "won") {
+      this.cx.fillStyle = "rgb(68, 191, 255)";
+    } else if (status == "lost") {
+      this.cx.fillStyle = "rgb(44, 136, 214)";
+    } else {
+      this.cx.fillStyle = "rgb(52, 166, 251)";
+    }
+    this.cx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  drawBackground(level) {
+    let { left, top, width, height } = this.viewport;
+    let xStart = Math.floor(left);
+    let xEnd = Math.ceil(left + width);
+    let yStart = Math.floor(top);
+    let yEnd = Math.ceil(top + height);
+
+    for (let y = yStart; y < yEnd; y++) {
+      for (let x = xStart; x < xEnd; x++) {
+        let tile = level.rows[y][x];
+        if (tile == "empty") continue;
+        let screenX = (x - left) * scale;
+        let screenY = (y - top) * scale;
+        let tileX = tile == "lava" ? scale : 0;
+        this.cx.drawImage(
+          otherSprites,
+          tileX,
+          0,
+          scale,
+          scale,
+          screenX,
+          screenY,
+          scale,
+          scale
+        );
+      }
+    }
+  }
+  drawPlayer(player, x, y, width, height) {
+    width += playerXOverlap * 2;
+    x -= playerXOverlap;
+    if (player.speed.x != 0) {
+      this.flipPlayer = player.speed.x < 0;
+    }
+    let tile = 8;
+    if (player.speed.y != 0) {
+      tile = 9;
+    } else if (player.speed.x != 0) {
+      tile = Math.floor(Date.now() / 60) % 8;
+    }
+    this.cx.save();
+    if (this.flipPlayer) {
+      flipHorizentally(this.cx, x + width / 2);
+    }
+    let tileX = tile * width;
+    this.cx.drawImage(
+      playerSprites,
+      tileX,
+      0,
+      width,
+      height,
+      x,
+      y,
+      width,
+      height
+    );
+    this.cx.restore();
+  }
+  drawActors(actors) {
+    for (let actor of actors) {
+      let width = actor.size.x * scale;
+      let height = actor.size.y * scale;
+      let x = (actor.pos.x - this.viewport.left) * scale;
+      let y = (actor.pos.y - this.viewport.top) * scale;
+      if (actor.type == "player") {
+        this.drawPlayer(actor, x, y, width, height);
+      } else {
+        let tileX = (actor.type == "coin" ? 2 : 1) * scale;
+        this.cx.drawImage(
+          otherSprites,
+          tileX,
+          0,
+          width,
+          height,
+          x,
+          y,
+          width,
+          height
+        );
+      }
+    }
+  }
+}
+let otherSprites = document.createElement("img");
+let playerSprites = document.createElement("img");
+playerSprites.src = "img/player.png";
+otherSprites.src = "./img/sprites.png";
+const playerXOverlap = 4;
+function flipHorizentally(contex, around) {
+  contex.translate(around, 0);
+  contex.scale(-1, 1);
+  contex.translate(-around, 0);
+}
 
 // running
 function runAnimation(frameFunc) {
