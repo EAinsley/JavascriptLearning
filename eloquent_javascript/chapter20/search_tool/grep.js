66b98b56efbc93fb04c2122b7996aa65d5a5d5c9
EAinsley <vhtmscyo@gmail.com>
2021-08-21 17:47:51 +0800

    finish chapter 20 exercise 1

--- eloquent_javascript/chapter20/search_tool/grep.js
+++ eloquent_javascript/chapter20/search_tool/grep.js
@@ -1,44 +1,59 @@
 const process = require("process");
-const { readFile } = require("fs").promises;
+const { readFile, stat, readdir } = require("fs").promises;
 
 const aim = new RegExp(process.argv[2]);
 
-if (process.argv.length > 3) {
-  /* file search
-    TODO: rewrite promise
-    TODO: skip duplicate files
-    TODO: restrain maximun file read
-    TODO: add error handler
+/* file search
+    TODO: filter file type
    */
-  let num_matched = 0;
-  let working = [];
+const working = [];
+if (process.argv.length > 3) {
+  const searched = [];
   for (let filename of process.argv.slice(3)) {
-    working.push(
-      readFile(filename, "utf-8")
-        .catch((error) => {
-          if (error.code != "ENOENT") return Promise.reject("Unknown error");
-          return Promise.reject(`No such file: ${filename}`);
-        })
-        .then((text) => {
-          if (aim.test(text)) {
-            num_matched++;
-            return { matched: true, filename };
-          }
-          return { matched: false, filename };
-        })
-    );
+    if (!searched.includes(filename)) {
+      searched.push(filename);
+      working.push(search(filename));
+    }
   }
+} else {
+  working.push(search("."));
+}
 
-  Promise.allSettled(working).then((values) => {
-    values.forEach((value) => {
-      if (value.status == "rejected") console.log(value.reason);
-      else if (value.value.matched)
-        console.log(`Matched: ${value.value.filename}`);
-    });
+Promise.allSettled(working).then((values) => {
+  values.forEach((value) => {
+    let num_matched = 0;
+    for (const result of value.value) {
+      if (result.status == "matched") {
+        console.log(`Matched: ${result.fileanme}`);
+        num_matched++;
+      } else if (result.status == "error") {
+        console.log(`Error: ${result.reason}`);
+      }
+    }
     if (num_matched) console.log(`totoal matched files: ${num_matched}`);
     else console.log("No match.");
   });
-} else {
-  /* recursive search */
-  console.log("Not Implemented.");
+});
+
+async function search(name) {
+  try {
+    let stats = await stat(name);
+    if (!stats.isDirectory()) {
+      const text = await readFile(name, "utf-8");
+      if (aim.test(text)) return [{ status: "matched", fileanme: name }];
+      return [{ status: "nomatch" }];
+    }
+    const files = await readdir(name);
+    const results = [];
+    for (const file of files) {
+      results.push(search(name + "/" + file));
+    }
+    return (await Promise.allSettled(results))
+      .reduce((flattened, item) => flattened.concat(item.value), [])
+      .filter((item) => item.status != "nomatch");
+  } catch (error) {
+    console.log("e1");
+    if (error.code != "ENOENT") throw error;
+    return [{ status: "error", reason: `No such file or directory: ${name}` }];
+  }
 }
