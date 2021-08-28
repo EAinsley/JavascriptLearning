51ecf706a0bd64a1af375bdf98e89fb9bdb375f7
EAinsley <vhtmscyo@gmail.com>
2021-08-28 18:06:34 +0800

    remove useless code

--- eloquent_javascript/chapter20/online_editor/www/sources/main.js
+++ eloquent_javascript/chapter20/online_editor/www/sources/main.js
@@ -9,13 +9,3 @@ function main() {
   document.body.appendChild(editor.dom);
   // save_button.addEventListener("click", savefile);
 }
-
-function elt(type, props, ...children) {
-  let dom = document.createElement(type);
-  if (props) Object.assign(dom, props);
-  for (let child of children) {
-    if (typeof child != "string") dom.appendChild(child);
-    else dom.appendChild(document.createTextNode(child));
-  }
-  return dom;
-}
