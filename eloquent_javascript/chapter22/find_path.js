48762b3fb39bf36da600bd248447f283fe45566f
EAinsley <vhtmscyo@gmail.com>
2021-09-12 21:06:10 +0800

    finish chapter22

--- eloquent_javascript/chapter22/find_path.js
+++ eloquent_javascript/chapter22/find_path.js
@@ -1,36 +1,34 @@
 function findPath(a, b) {
-  const visited = [a];
-  const pathChain = [0];
-  let currentNode = a;
-  let currentNodePosition = 0;
-  while (currentNode != b) {
-    for (const neibour of currentNode.edges) {
-      if (!visited.includes(neibour)) {
-        visited.push(neibour);
-        pathChain.push(currentNodePosition);
+  const visited = new Set([a]);
+  const path_queue = [[a]];
+  for (let i = 0; i < path_queue.length; i++) {
+    const path = path_queue[i];
+    const end = path[path.length - 1];
+    if (end == b) return path;
+    for (const neibour of end.edges) {
+      if (!visited.has(neibour)) {
+        visited.add(neibour);
+        path_queue.push(path.concat(neibour));
       }
     }
-    currentNodePosition++;
-    currentNode = visited[currentNodePosition];
   }
-  const path = [b];
-  while (currentNodePosition != 0) {
-    currentNodePosition = pathChain[currentNodePosition];
-    path.unshift(visited[currentNodePosition]);
-  }
-  return path;
+  return null;
 }
 
 // testcases
 console.log("---find path---");
-let graph = treeGraph(4, 4);
+let graph = treeGraph(6, 6);
 let root = graph[0],
   leaf = graph[graph.length - 1];
 console.log(findPath(root, leaf).length);
-// → 4
 
 leaf.connect(root);
 console.log(findPath(root, leaf).length);
-// → 2
+console.log("---end---");
 
+console.log("---Timing---");
+let graph2 = treeGraph(6, 15);
+let start = Date.now();
+console.log(findPath(graph2[0], graph2[graph2.length - 1]).length);
+console.log(Date.now() - start);
 console.log("---end---");
